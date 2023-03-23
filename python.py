f4f0a9a376c427b1beb405eab970c400bd6865c9
RagavN12 <96319441+RagavN12@users.noreply.github.com>
2023-03-23 00:34:49 +0000

    hola buenosdias como savas

--- python.py
+++ python.py
@@ -1,1 +1,7 @@
-print("helloWorld")
+from manim import *
+
+class CreateCircle(Scene):
+    def construct(self):
+        circ = Circle()
+        circ.set_fill(RED,opacity=0.5)
+        self.play(Create(circ))
